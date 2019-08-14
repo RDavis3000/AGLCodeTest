27628bc993a61a4e3e8c22f40f0b95c5dd3c9cde
Richard <richard.davis@outlook.com.au>
2019-08-14 16:53:06 +1000

    adding unit tests

---
+++ PetOwnerServiceTests/CatsAlphaOwnerGenderServiceTest.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using PetOwnerModels;
+using PetOwnerService;
+using PetOwnerService.PetOwnerProcessors;
+using PetOwnerService.Readers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetOwnerServiceTests
+{
+    [TestClass]
+    public class CatsAlphaOwnerGenderServiceTest
+    {
+
+        [TestMethod]
+        public async void GetCatsAlphaOwnerGenderDictionaryTest()
+        {
+            //arrange
+            List<PetOwner> testData = new List<PetOwner>()
+            {
+                new PetOwner()
+                {
+                    Gender="Gender1",
+                    Pets = new List<Pet>()
+                    {
+                        new Pet()
+                        {
+                            Name="aPet1",
+                            Type="Cat"
+                        },
+                        new Pet()
+                        {
+                            Name="dupe1",
+                            Type="Cat"
+                        },
+                        new Pet()
+                        {
+                            Name="cPet3",
+                            Type="Type2"
+                        }
+                    }
+                },
+                new PetOwner()
+                {
+                    Gender="Gender1",
+                    Pets = new List<Pet>()
+                    {
+                        new Pet()
+                        {
+                            Name="abPet4",
+                            Type="Cat"
+                        },
+                        new Pet()
+                        {
+                            Name="bzPet5",
+                            Type="Cat"
+                        },
+                        new Pet()
+                        {
+                            Name="caPet6",
+                            Type="Type2"
+                        }
+                    }
+                },
+                new PetOwner()
+                {
+                    Gender="Gender2",
+                    Pets = new List<Pet>()
+                    {
+                        new Pet()
+                        {
+                            Name="dupe1",
+                            Type="Cat"
+                        },
+                        new Pet()
+                        {
+                            Name="bPet7",
+                            Type="Cat"
+                        },
+                        new Pet()
+                        {
+                            Name="bPet8",
+                            Type="Cat"
+                        },
+
+                    }
+                },
+            };
+
+            var expectedresult = new Dictionary<string, List<string>>()
+            {
+
+                {"Gender1", new List<string>()
+                {
+                   "aPet1",
+                   "abPet4",
+                   "bzPet5",
+                   "dupe1"
+                }
+                },
+                {"Gender2", new List<string>()
+                {
+                   "bPet7",
+                   "bPet8",
+                   "dupe1"
+                }
+                }
+            };
+
+            var IReadPetOwnersMock = new Mock<IReadPetOwners>();
+
+            IReadPetOwnersMock.Setup(p => p.ReadPetOwners()).ReturnsAsync(testData);
+
+            var ownerProcessor = new ProcessCatsAlphaOwnerGender();
+
+            var catsAlphaOwnerGenderService = new CatsAlphaOwnerGenderService(IReadPetOwnersMock.Object, ownerProcessor);
+
+            //act
+            var result = await catsAlphaOwnerGenderService.GetCatsAlphaOwnerGenderDictionaryAsync();
+
+            //assert
+            Assert.AreEqual(result, expectedresult);
+        }
+    }
+}
---
+++ PetOwnerServiceTests/ProcessCatsAlphaOwnerGenderTest.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using PetOwnerModels;
+using PetOwnerService.PetOwnerProcessors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetOwnerServiceTests
+{
+    [TestClass]
+    public class ProcessCatsAlphaOwnerGenderTest
+    {
+
+        [TestMethod]
+        public void TestProcess()
+        {
+
+            //Arrange
+
+            var processCatsAlphaOwnerGender = new ProcessCatsAlphaOwnerGender();
+
+            List<PetOwner> testData = new List<PetOwner>()
+            {
+                new PetOwner()
+                {
+                    Gender="Gender1",
+                    Pets = new List<Pet>()
+                    {
+                        new Pet()
+                        {
+                            Name="aPet1",
+                            Type="Cat"
+                        },
+                        new Pet()
+                        {
+                            Name="dupe1",
+                            Type="Cat"
+                        },
+                        new Pet()
+                        {
+                            Name="cPet3",
+                            Type="Type2"
+                        }
+                    }
+                },
+                new PetOwner()
+                {
+                    Gender="Gender1",
+                    Pets = new List<Pet>()
+                    {
+                        new Pet()
+                        {
+                            Name="abPet4",
+                            Type="Cat"
+                        },
+                        new Pet()
+                        {
+                            Name="bzPet5",
+                            Type="Cat"
+                        },
+                        new Pet()
+                        {
+                            Name="caPet6",
+                            Type="Type2"
+                        }
+                    }
+                },
+                new PetOwner()
+                {
+                    Gender="Gender2",
+                    Pets = new List<Pet>()
+                    {
+                        new Pet()
+                        {
+                            Name="dupe1",
+                            Type="Cat"
+                        },
+                        new Pet()
+                        {
+                            Name="bPet7",
+                            Type="Cat"
+                        },
+                        new Pet()
+                        {
+                            Name="bPet8",
+                            Type="Cat"
+                        },
+
+                    }
+                },
+            };
+
+            var expectedresult = new Dictionary<string, List<string>>()
+            {
+
+                {"Gender1", new List<string>()
+                {
+                    "abPet4",
+                    "aPet1",
+                    "bzPet5",
+                    "dupe1"
+                }
+                },
+                {"Gender2", new List<string>()
+                {
+                   "bPet7",
+                   "bPet8",
+                   "dupe1"
+                }
+                }
+            };
+
+            //Act
+
+            var processResult = processCatsAlphaOwnerGender.Process(testData);
+
+            //Assert
+            CollectionAssert.AreEqual(processResult, expectedresult);            
+        }
+    }
+}
